42db3653d1b01ff3f317f4beacf215287099b604
Jayesh Binwal <binwaljayesh@gmail.com>
2023-12-13 14:04:01 +0530

    loops in ruby

---
+++ loops.rb
@@ -0,0 +1,32 @@
+bomb_timer=0
+while bomb_timer <=5
+    puts bomb_timer
+    bomb_timer +=1
+end
+speed_run = 6
+while speed_run >=0
+    puts "speed_run"
+    speed_run -=3
+end
+
+time_out=15
+until time_out == 10
+    puts "Time_out"
+    time_out -=5
+end
+hp=10
+loop do
+    hp -=2
+    puts "damage do andar aao, jiggle de jiggle"
+    break if 
+    hp <= 2
+    end
+    puts "Masla ho gaya ji"
+
+    i = "Sudo Placements"
+for a in 1..5 do
+     
+ puts i
+  
+end
+    
